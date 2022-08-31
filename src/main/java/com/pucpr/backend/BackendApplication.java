fbee6abc50993505ace50cd1ee0d63903f25ad2d
StreetDanceD313 <dudu020300@gmail.com>
2022-08-31 20:47:11 -0300

    Removido Eureka

--- src/main/java/com/pucpr/backend/BackendApplication.java
+++ src/main/java/com/pucpr/backend/BackendApplication.java
@@ -2,11 +2,8 @@ package com.pucpr.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BackendApplication {
 
     public static void main(String[] args) {
